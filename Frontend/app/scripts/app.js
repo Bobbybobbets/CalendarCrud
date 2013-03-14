6ba6f858f0da6966fcc724b10218160550809c2f
Patrice Paquette <paquette.patrice@gmail.com>
2013-03-14 04:21:45 -0400

    Set up angularjs and angular-ui. Set up fullCalendar, but did not connect to the server yet

---
+++ Frontend/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var FrontendApp = angular.module('FrontendApp', ['ui'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
